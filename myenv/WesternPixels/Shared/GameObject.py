cbd9d3249b1fd3b58b71f6e5293da533eb63902d
Knapecz Ádám <knapecz.adam@gmail.com>
2016-09-16 21:23:50 +0200

    Add two parameters to constructor

--- myenv/WesternPixels/Shared/GameObject.py
+++ myenv/WesternPixels/Shared/GameObject.py
@@ -3,9 +3,11 @@ import pygame
 
 class GameObject(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, image, position):
         pygame.sprite.Sprite.__init__(self)
-        self.__sprite = None
+        self.__sprite = image
+        self.__rect = image.get_rect()
+        self.__position = position
         self.__mask = None
         self.__sound = None
 
